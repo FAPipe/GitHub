d5aa6409d03ba15571f34bc2b0e5bde5d44deb86
jpknap <jp.knap@gmail.com>
2014-01-03 17:55:34 -0300

    prueba movimiento

---
+++ Assets/mov.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mov : MonoBehaviour {
+	public GameObject avion;
+	int dere = 0;
+	// Use this for initialization
+	void Start () {
+
+	
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKey(KeyCode.D)) {
+			dere= dere-1;
+			avion.transform.Translate(Vector3.right * Time.deltaTime*6 );
+
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			
+			avion.transform.Translate(Vector3.down * Time.deltaTime*6);
+		}
+		if (Input.GetKey(KeyCode.A)) {
+			
+			avion.transform.Translate(Vector3.left * Time.deltaTime*6);		
+		}
+		if (Input.GetKey(KeyCode.W)) {
+			
+			avion.transform.Translate(Vector3.up * Time.deltaTime*6);
+		}
+	
+	}
+}
