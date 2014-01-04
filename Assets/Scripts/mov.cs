5e7ce98d6ca933543b13a0b2072b1431626920d2
jpknap <jp.knap@gmail.com>
2014-01-04 10:57:30 -0300

    Fisica elementos sobre el avion
    Se encuentra comentado el codigo

--- Assets/Scripts/mov.cs
+++ Assets/Scripts/mov.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class mov : MonoBehaviour {
 	public GameObject avion;
-	int dere = 0;
+	public int velocidad;
 	// Use this for initialization
 	void Start () {
+		// velocidad de movimiento
+		velocidad = 6;
 
 	
 	
@@ -13,23 +15,24 @@ public class mov : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		//pregunta si se encuentra  alguna de las teclas presionadas y se realiza un movimiento dependiente de estas
 	
 		if (Input.GetKey(KeyCode.D)) {
-			print("D");
-			avion.transform.Translate(Vector3.right * Time.deltaTime*6 );
+
+			avion.transform.Translate(Vector3.right * Time.deltaTime*velocidad );
 
 		}
 		if (Input.GetKey(KeyCode.S)) {
 			
-			avion.transform.Translate(Vector3.down * Time.deltaTime*6);
+			avion.transform.Translate(Vector3.down * Time.deltaTime*velocidad);
 		}
 		if (Input.GetKey(KeyCode.A)) {
 			
-			avion.transform.Translate(Vector3.left * Time.deltaTime*6);		
+			avion.transform.Translate(Vector3.left * Time.deltaTime*velocidad);		
 		}
 		if (Input.GetKey(KeyCode.W)) {
 			
-			avion.transform.Translate(Vector3.up * Time.deltaTime*6);
+			avion.transform.Translate(Vector3.up * Time.deltaTime*velocidad);
 		}
 	
 	}
