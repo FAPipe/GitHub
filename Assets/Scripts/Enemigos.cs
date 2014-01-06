c2f6d8b75406f0bbe111be7800e46d64ba790db9
FAPipe <fclaude@alumnos.uach.cl>
2014-01-06 09:55:58 -0300

    probando nuevo proyecto

---
+++ Assets/Scripts/Enemigos.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemigos : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	int velocidadEnemigo = 5;
+	// Update is called once per frame
+	void Update () {
+		//Mover enemigos en el eje z, osea, que aparezcan estos desde el fondo de la pantalla hacia la nave.
+		transform.Translate (0, 0, -velocidadEnemigo * Time.deltaTime);
+
+		//Si los enemigos salen de la pantalla, estos vuelven a aparecer, desde el fondo.
+		if(transform.position.z <= -7){
+			Vector3 temp = transform.position; 
+			temp.z = 10;
+			temp.x=Random.Range(-8,8);
+			temp.y=Random.Range(-4,4);
+			transform.position = temp;
+		}
+	}
+}
+
---
+++ Assets/Scripts/FisicaAvion.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FisicaAvion : MonoBehaviour {
+
+	// variables publicas de las condiciones climaticas
+	public bool viento_este;
+	public bool viento_oeste;
+	public bool lluvia_estado;
+	public GameObject avion;
+	int velocidad_elementos;
+	void Start () {
+		//velocidad inicial del movimeinto de los elementos es 3, comienza el juego sin ningun elemento en el campo
+		velocidad_elementos = 3;
+		viento_este=false;
+		viento_oeste=false;
+		lluvia_estado=false;
+
+	
+	}
+	
+	// Se pregunta que condiciones se encuentran activadas y se mueve el objeto en una direccion dependiente de estos
+	void Update () {	
+		if(viento_este){			
+			avion.transform.Translate (Vector3.right * Time.deltaTime * velocidad_elementos);
+		}	
+		if(viento_oeste){			
+			avion.transform.Translate (Vector3.left * Time.deltaTime * velocidad_elementos);
+		}	
+		if(lluvia_estado){			
+			avion.transform.Translate (Vector3.down * Time.deltaTime * velocidad_elementos);
+		}		
+		avion.transform.Translate (Vector3.down * Time.deltaTime * 1);
+	}
+}
