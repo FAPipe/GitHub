0079831b5518874b9f7dd8761ae83ce399fd1da9
FAPipe <fclaude@alumnos.uach.cl>
2014-01-06 10:46:07 -0300

    creando controllador y script objetos

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+//objeto que se encarga de controlar los movimientos y el juego
+public class Controller : MonoBehaviour {
+	public float velocidadObjetos;
+	public float momentumAvion;
+	public int nivel;
+	public int maxObjetos;
+	public int cantObjetos;
+	// Use this for initialization
+	void Start () {
+		velocidadObjetos=10f;
+		momentumAvion=0f;
+		nivel =1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/movimientoObjetos.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movimientoObjetos : MonoBehaviour {
+	public Controller controller;
+	float velocidad;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		velocidad = controller.velocidadObjetos;
+		transform.position = new Vector3 (transform.position.x, transform.position.y,transform.position.z);
+	}
+}
