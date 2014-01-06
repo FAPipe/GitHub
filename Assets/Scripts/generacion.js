49c7e3f37b086318eb207c83ff57bba1909d22ef
FAPipe <fclaude@alumnos.uach.cl>
2014-01-06 09:33:23 -0300

    sigo probando ingnore

---
+++ Assets/Scripts/generacion.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+var objetoAInstanciar:GameObject;
+var numeroDeInstancias:int; 
+var velocidadDeGeneracion:int; 
+private var rot:Quaternion = Quaternion.identity; 
+private var nInstancias:int = 0; 
+private var tiempoDeGeneracion : float; 
+function Update () { if((nInstancias <= numeroDeInstancias) && (Time.time > tiempoDeGeneracion)) { 
+	nInstancias += 1; tiempoDeGeneracion = Time.time + velocidadDeGeneracion; 
+	var objeto:GameObject = Instantiate(objetoAInstanciar,gameObject.transform.position,rot); 
+} 
+}
