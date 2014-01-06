2d18f386a80c641a4d7c77aa550fcb6d0a550318
jpknap <jp.knap@gmail.com>
2014-01-06 12:39:29 -0300

    lluvia

---
+++ Assets/Scripts/trigger_lluvia.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trigger_lluvia : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		print ("entro al trigger");
+	}
+
+	
+	void OnTriggerStay (Collider other){
+		print ("esta dentro del trigger");
+	}
+	void OnTriggerExit (Collider other){
+		print ("sali del trigger");
+	}
+}
